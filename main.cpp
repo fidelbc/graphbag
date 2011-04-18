69dc3eee76cff5ae3d4f52067ea905fb46bea90e
Fidel Barrera-Cruz <fidel.barrera@gmail.com>
2011-04-18 18:04:41 -0400

    Added vertex class and compiling file

--- main.cpp
+++ main.cpp
@@ -1,24 +1,93 @@
+///////////////////////////////////////////////////////////////////////////
+// This program is free software: you can redistribute it and/or modify	 //
+// it under the terms of the GNU General Public License as published by	 //
+// the Free Software Foundation, either version 3 of the License, or	 //
+// (at your option) any later version.					 //
+//   									 //
+// This program is distributed in the hope that it will be useful,	 //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of	 //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 //
+// GNU General Public License for more details.				 //
+// 									 //
+// You should have received a copy of the GNU General Public License	 //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>. //
+///////////////////////////////////////////////////////////////////////////
+
 #include <QtGui>
 
 #include<QGraphicsView>
 #include<QGraphicsScene>
 #include<QGraphicsEllipseItem>
 
-#include<iostream>
 
-using namespace std;
+#include<QDebug>
 
-void print_pointf( QPointF * p ){
+class Vertex : public QGraphicsEllipseItem
+{
 
-  cout <<"(" << p->x() << ", " << p->y() << ")";
+public:
 
-}
+  Vertex( double x, double y, double r=20.0 )
+    : QGraphicsEllipseItem( x-r/2, y-r/2, r, r ), radius( r )
+  {
 
-void print_point( QPoint * p ){
+    //setPos( x, y );
+    setFlag( QGraphicsItem::ItemIsMovable, true );
 
-  cout <<"(" << p->x() << ", " << p->y() << ")";
+    //setFlag( QGraphicsItem::ItemIsSelectable, true );
 
-}
+    pen_color.setRgb( 88, 61, 164 );
+    brush_color.setRgb( 154, 204, 50 );
+
+    QPen pen(Qt::SolidLine);
+    pen.setColor( Qt::black );
+    pen.setWidth( 2 );
+
+    setPen( pen );
+    
+    //QRadialGradient gradient(x, y, radius, x, y);
+    //gradient.setColorAt(1, brush_color );
+    //gradient.setColorAt(0, QColor::fromRgbF(0, 0, 0, 1));
+    QBrush brush( Qt::darkGreen );
+    setBrush( brush );
+
+  }
+
+
+  void mousePressEvent ( QGraphicsSceneMouseEvent * mouseEvent )
+  {
+
+    QPointF pos(mouseEvent->pos());
+    QPointF scene_pos( mouseEvent->scenePos());
+      
+    qDebug() << "Press in Vertex.";
+    qDebug() <<"\tpos: " << pos;
+    qDebug() <<"\tscene_pos: "<< scene_pos;
+    qDebug() <<"\tItem currently at pos: " << this->pos();
+    qDebug() <<"\tItem currently at scene_pos: "<< scenePos();
+
+    qDebug();
+    
+    QGraphicsEllipseItem::mousePressEvent( mouseEvent );  
+
+  }
+
+
+  enum { Type = UserType + 1 };
+
+  int type() const
+  {
+    return Type;
+  }
+
+private:
+
+  double radius;
+  QColor pen_color;
+  QColor brush_color;
+
+
+};
 
 class GraphScene : public  QGraphicsScene
 {
@@ -36,20 +105,23 @@ public:
     QPointF scene_pos( mouseEvent->scenePos());
     QPoint screen_pos( mouseEvent->screenPos());
       
-    cout << "Press in Scene."<< endl;
-    cout <<"\tpos: ";print_pointf( &pos );cout<<endl;
-    cout <<"\tscene_pos: ";print_pointf( &scene_pos );cout<<endl;
-    cout <<"\tscreen_pos: ";print_point( &screen_pos );cout<<endl;
+    qDebug() << "Press in Scene.";
+    qDebug() <<"\tpos: " << pos;
+    qDebug() <<"\tscene_pos: "<< scene_pos;
+    qDebug() <<"\tscreen_pos: "<< screen_pos;
+    
+    qDebug();
+
+    if( itemAt( scene_pos ) ){
 
     QGraphicsScene::mousePressEvent( mouseEvent );  
 
-    double r=10;
+    }else{
 
-    QGraphicsEllipseItem * vx = new QGraphicsEllipseItem( scene_pos.x()-r/2, scene_pos.y()-r/2, r, r );
-
-    vx-> setFlag( QGraphicsItem::ItemIsMovable, true );
-
-    addItem( vx );
+      Vertex* vx = new Vertex( scene_pos.x(), scene_pos.y() );
+    
+      addItem( vx );
+    }
 
   }
 
@@ -60,7 +132,10 @@ class GraphView :
 {
 
 public:
-  
+  GraphView(): QGraphicsView()
+  {
+    setRenderHint(QPainter::Antialiasing);
+  }
 
   void mousePressEvent ( QMouseEvent * event )
   {
@@ -68,8 +143,8 @@ public:
 
     QPoint pos(event->pos());
 
-    cout << "Press in View." << endl;
-    cout << "\tpos:"; print_point( &pos );  cout<<endl;
+    qDebug() << "Press in View.";
+    qDebug() << "\tpos: "<< pos;
 
     QGraphicsView::mousePressEvent( event );  
 
@@ -88,11 +163,7 @@ int main(int argc, char **argv)
   GraphScene scene( &view );
 
   view.setScene( &scene );
-
-  //int n =10;
-  QGraphicsEllipseItem * vx = new QGraphicsEllipseItem( 10.0, 10.0, 10.0, 10.0 );
-
-  scene.addItem( vx );
+ 
   
   scene.addLine(-100,0,0,0);
   scene.addLine(0,0,100,0);
